0d4ef991314dd4881f53d8db40f8caafb2726009
SenSohom <ss4887@njit.edu>
2026-04-20 20:38:21 -0400

    Change job quality of service and GPU type

--- HPC/Code-HPC/job.sh
+++ HPC/Code-HPC/job.sh
@@ -3,11 +3,11 @@
 #SBATCH --output=%x.%j.out # %x.%j expands to slurm JobName.JobID
 #SBATCH --error=%x.%j.err
 #SBATCH --partition=course_gpu
-#SBATCH --qos=standard
+#SBATCH --qos=course
 #SBATCH --account=2026-spring-ece-381-th36-ucid
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100_10g:1
 #SBATCH --time=59:00  # D-HH:MM:SS
 #SBATCH --mem-per-cpu=4000M
 
